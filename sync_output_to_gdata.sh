e6bb3e257174a769c4fcf29ca65c7a8eaabd0368
Andy Hogg <Andy.Hogg@anu.edu.au>
2020-01-12 15:45:44 +1100

    2020-01-12 15:45:43: Run 331

--- sync_output_to_gdata.sh
+++ sync_output_to_gdata.sh
@@ -2,6 +2,7 @@
 #PBS -q express
 #PBS -l ncpus=1
 #PBS -l wd
+#PBS -l storage=gdata/hh5
 #PBS -l walltime=4:00:00,mem=2GB
 #PBS -P v45
 #PBS -N output_to_gdata
@@ -9,7 +10,7 @@
 # Set this directory to something in /g/data3/hh5/tmp/cosima/
 # Make a unique path for your set of runs.
 # DOUBLE-CHECK IT IS UNIQUE SO YOU DON'T OVERWRITE EXISTING OUTPUT!
-GDATADIR=/g/data3/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf9091_gadi_retest
+GDATADIR=/g/data/hh5/tmp/cosima/access-om2-01/01deg_jra55v13_ryf9091_gadi_retest
 
 mkdir -p ${GDATADIR}
 cd archive
