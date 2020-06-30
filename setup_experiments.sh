a2cafec86ec3c569834cc30b51bf8cfa22b0a09c
Paul Leopardi <paul.leopardi@anu.edu.au>
2020-06-30 11:48:19 +1000

    Link to archive directory. Checkout original branch.

--- setup_experiments.sh
+++ setup_experiments.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 set -eux
+if [ -d "$ACCESS_OM2_DIR" ]
+then
+  default_archive_dir=$(cd "$ACCESS_OM2_DIR/../archive"; pwd)
+else
+  default_archive_dir=$(cd "../../../archive"; pwd)
+fi
+archive_dir=${1:-$default_archive_dir}
 DRYRUN=""
 orig_exp=$(basename $(pwd))
+orig_branch=$(git branch | grep '*' | cut -d' ' -f2)
 config_branches=$(git branch -a | grep config)
+$DRYRUN ln -s -f $archive_dir archive
 for branch in $config_branches
 do 
   config=$(basename $branch)
@@ -13,4 +22,4 @@ do
     $DRYRUN git checkout $branch
   $DRYRUN popd
 done
-
+$DRYRUN git checkout $orig_branch
